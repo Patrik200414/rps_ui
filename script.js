3712ac6e35bd8c90955416a078e6e38a6d5f1af2
Patrik200414 <martinpatrik14@gmail.com>
2022-12-26 10:35:31 +0100

    Make the rock button work
    I created a play function wich runs when you click the rock button
    In the beggining we have a state, which will change their attributes
    If the state.playGame is false it remove the elements int results
    If it's true it will play the game
    It checks if you choose rock, paper or scissors and it will render the right picture and text to it
    Also it's append them to the result div and in the end it changes the playGround div

---
+++ script.js
@@ -0,0 +1,65 @@
+let state = {
+    playGame: false,
+    totalScoreP: 0,
+    totalScoreC: 0,
+    choices: ['rock','paper','scissors']
+}
+
+
+
+document.querySelector('#rock').addEventListener('click', (event) => {
+    state.playGame = true;
+    play('rock');
+}) 
+
+
+function play(choice){
+    if(state.playGame === false){
+        document.querySelector('#playGround').removeChild();
+    }
+    else{
+        let playGround = document.querySelector('#result');
+        let divResult = document.createElement('div');
+        divResult.setAttribute('class','result');
+        divResult.setAttribute('id', 'result');
+
+        //player choice
+        let divPlayer = document.createElement('div');
+        divPlayer.setAttribute('class','player');
+        divPlayer.setAttribute('id', 'player');
+
+        let h2Player = document.createElement('h2');
+        switch(choice.toLowerCase()){
+            case 'rock':
+                h2Player.textContent = 'Rock';
+                break;
+            case 'paper': 
+                h2Player.textContent = 'Paper';
+                break;
+            case 'scissor':
+                h2Player.textContent = 'Scissors';
+                break;
+        }
+
+        let imgPlayer = document.createElement('img');
+        
+        switch(choice.toLowerCase()){
+            case 'rock':
+                imgPlayer.setAttribute('src', 'https://thumbs.dreamstime.com/b/cartoon-rock-pebbles-digital-illustration-147192308.jpg')
+                break;
+            case 'paper': 
+                imgPlayer.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVqx-rJKSD_PpQMUv10YgkoF-akjUeo7fBlg&usqp=CAU')
+                break;
+            case 'scissor':
+                imgPlayer.setAttribute('src', 'https://img.freepik.com/premium-vector/vector-doodle-hand-drawn-illustration-cartoon-scissors-isolated-white_364586-724.jpg?w=2000')
+                break;
+        }
+
+        divPlayer.append(h2Player, imgPlayer);
+        divResult.append(divPlayer);
+
+        playGround.replaceWith(divResult);
+
+    }
+}
+
